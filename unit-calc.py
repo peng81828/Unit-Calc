99c48bdde825681f8178dc4da05eac08dd52c0d9
peng81828 <peng81828@users.noreply.github.com>
2015-08-21 19:18:26 -0400

    Simpler code, clean up

--- unit-calc.py
+++ unit-calc.py
@@ -1,5 +1,7 @@
 print ("Peng81828's simple individual price calculator")
+
 totalPrice = raw_input("Enter the total price of your item here: $ ")
-individual = raw_input("How many units does your package bring?")
+individual = raw_input("How many units does your package bring? ")
+
 price = (int(totalPrice) / int(individual))
 print "The price per unit is $" + str(price)
