9c5c2403fc71b1f9167cfcdb80e74eac11b3179c
peng81828 <peng81828@users.noreply.github.com>
2015-08-21 19:14:49 -0400

    Update unit-calc.py

--- unit-calc.py
+++ unit-calc.py
@@ -1,18 +1,5 @@
 print ("Peng81828's simple individual price calculator")
-goodinput = False
-while not goodinput:
-	try:
-		input_var = float(raw_input("Enter the total price of your item here: $ "))
-		goodinput = True
-	except ValueError:
-		print ("Invalid Input! Are you sure that's a number?")
-goodinput2 = False
-while not goodinput2:
-	try:
-		individual = float(raw_input("How many units does your package bring? "))
-		goodinput2 = True
-	except ValueError:
-		print ("Invalid Input! Are you sure that's a number?")
-price= (input_var / individual)
-price= round (price,2)
-print ("The price per unit is $"),price
+totalPrice = raw_input("Enter the total price of your item here: $ ")
+individual = raw_input("How many units does your package bring?")
+price = (int(totalPrice) / int(individual))
+print "The price per unit is $" + str(price)
